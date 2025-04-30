1667ba7f1a981a6c65ea5724cc2cf9841c1f29ac
Lucas M Nunes <105028691+lucasdevnunes@users.noreply.github.com>
2025-04-29 21:06:19 -0300

    Criacao das models iniciais e migracao do banco de dados

--- news/models.py
+++ news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    titulo = models.CharField(max_length=200)
+    resumo = models.TextField()
+    conteudo = models.TextField()
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo    
